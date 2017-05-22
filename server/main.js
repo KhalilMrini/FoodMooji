d40ee318a1262706dd6796004d46aff29ad5f7ca
KhalilMrini <Khalil@tsf-428-wpa-1-007.epfl.ch>
2017-05-22 13:52:25 +0200

    Reduced data significantly by computing sums

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
 import '../imports/api/tweets.js'
+import '../imports/api/countryfoodlist.js'
 
 Meteor.startup(() => {
   // code to run on server at startup
