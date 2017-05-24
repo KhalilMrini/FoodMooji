8f8c95009aca671a661fb56bb09fe25ad8552345
x4nd3r <x4nd3r@users.noreply.github.com>
2017-05-24 20:36:33 +0200

    Process data to key:value Objects, remove chart junk

--- imports/ui/SplitScreen.jsx
+++ imports/ui/SplitScreen.jsx
@@ -25,11 +25,11 @@ function processData() {
 
   for (var i = 1; i < raw_lines.length; i++) {
     var elements = raw_lines[i].split(',')
-    tarr = [];
+    var object = { };
     for (var j = 0; j < headers.length; j++) {
-      tarr.push(headers[j]+":"+elements[j]);
+      object[headers[j]] = elements[j];
     }
-    lines.push(tarr);
+    lines.push(object);
   }
   return lines;
 }
@@ -54,45 +54,6 @@ function processCountries() {
   }
   return object
 }
-
-////////////////////////////////////////////////////////////////////////////////
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height"),
-    radius = Math.min(width, height) / 2,
-    g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-var color = d3.scaleOrdinal(["#e4e429", "#29e487", "#29e429", "#29e4e4", "#2929e4", "#e42987", "#e42929", "#e48729"]);
-
-var pie = d3.pie()
-    .sort(null)
-    .value(function(d) { return d.sum; });
-
-var path = d3.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(0);
-
-var label = d3.arc()
-    .outerRadius(radius - 40)
-    .innerRadius(radius - 40);
-
-function drawPie(data, country, food) {
-  var arc = g.selectAll(".arc")
-    .data(pie(data.filter(function(d) { return d.country == country; })))
-    .enter().append("g")
-    .attr("class", "arc");
-  console.log(data);
-
-  arc.append("path")
-      .attr("d", path)
-      .attr("fill", function(d) { return color(d.data.emotion); });
-
-  arc.append("text")
-      .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
-      .attr("dy", "0.35em")
-      .text(function(d) { return d.data.sum; });
-}
-////////////////////////////////////////////////////////////////////////////////
 
 export default class SplitScreen extends Component {
 
