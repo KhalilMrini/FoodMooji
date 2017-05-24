4932710017bd81420dc5f7ab23b6d1f1454ac1f9
x4nd3r <x4nd3r@users.noreply.github.com>
2017-05-24 19:37:42 +0200

    Plot pie chart with per-country filtering

--- client/pie.js
+++ client/pie.js
@@ -8,7 +8,7 @@ var color = d3.scaleOrdinal(["#e4e429", "#29e487", "#29e429", "#29e4e4", "#2929e
 
 var pie = d3.pie()
     .sort(null)
-    .value(function(d) { return d.sum; });
+    .value(function(d) { return d.value; });
 
 var path = d3.arc()
     .outerRadius(radius - 10)
@@ -18,19 +18,39 @@ var label = d3.arc()
     .outerRadius(radius - 40)
     .innerRadius(radius - 40);
 
-function drawPie(data, country, food) {
+var country = "Australia";
+d3.csv("../data/tweets_data.csv", function(d) {
+  d.sum = +d.sum;
+  d.hour = +d.hour;
+  d.emotion = +d.emotion;
+  return d;
+}, function(error, data) {
+  if (error) throw error;
+
+  var byCountry = data.filter(function(d) {
+    if (d["country"] == country) {
+      return d;
+    }
+  });
+  console.log(byCountry);
+
+  var byEmotion = d3.nest()
+    .key(function(d) { return d.emotion; })
+    .rollup(function(v) { return d3.sum(v, function(d) { return d.sum; }); })
+    .entries(byCountry);
+  console.log(byEmotion);
+
   var arc = g.selectAll(".arc")
-    .data(pie(data))
+    .data(pie(byEmotion))
     .enter().append("g")
-    .filter(function(d) { return d.country == country; })
-    .attr("class", "arc");
+      .attr("class", "arc");
 
   arc.append("path")
       .attr("d", path)
-      .attr("fill", function(d) { return color(d.data.emotion); });
+      .attr("fill", function(d) { return color(d.data.key); });
 
   arc.append("text")
       .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
       .attr("dy", "0.35em")
-      .text(function(d) { return d.data.sum; });
-}
+      .text(function(d) { return d.data.value; });
+});
